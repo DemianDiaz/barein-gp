9960abce49fdb341cff4c098dd98ae3c4ef8c7ca
Demian Diaz <pddiaz19@outlook.com>
2023-12-08 23:59:00 -0300

    Agregué nuevas funciones al carrito de compras, icono de carrito y mensajes en navegador

--- js/app.js
+++ js/app.js
@@ -34,13 +34,29 @@ function mostrarArticulos(articulosFiltrados) {
 function filtrarArticulos() {
     const textoBusqueda = document.getElementById("buscadorArticulo").value.toLowerCase();
     const articulosFiltrados = articulos.filter((articulo) => articulo.nombre.includes(textoBusqueda));
-    mostrarArticulos(articulosFiltrados);
+    if (articulosFiltrados.length > 0) {
+        mostrarArticulos(articulosFiltrados);
+    } else {
+        mostrarMensajeNoResultados();
+    }
+}
+
+function mostrarMensajeNoResultados() {
+    const tienda = document.getElementById("tienda");
+    tienda.innerHTML = `<p class="blockquote" style="color: white; text-align: center; font-size: 2rem;">No hay resultados para la búsqueda.</p>`;
 }
 
 function agregarAlCarrito(nombre, precio, img) {
-    carrito.push({nombre, precio, img}); 
+    const articuloExistente = carrito.find((articulo) => articulo.nombre === nombre);
+    if (articuloExistente) {
+        articuloExistente.cantidad++;
+    } else {
+        carrito.push({nombre, precio, img, cantidad: 1});
+    }
     actualizarListaCarrito();
+    mostrarCantItems();
     mostrarModal();
+    sumarTotal()
     guardarEnLs();
 }
 
@@ -50,10 +66,13 @@ function actualizarListaCarrito() {
     carrito.map((articulo, index) => {
         let item = document.createElement('li');
         item.classList.add('list-group-item');
+        if (articulo.cantidad === undefined) {
+            articulo.cantidad = 1;
+        }
         item.innerHTML = 
             `<div style="display: flex; justify-content: space-between; align-items: center;">
                 <div style="margin-right: 10px;">
-                    <img src="${articulo.img}" alt="Imagen de ${articulo.nombre}" style="max-width: 50px; max-height: 50px;"> - ${articulo.nombre} - $ ${articulo.precio} - Subtotal $ ${(articulo.precio)}
+                    <img src="${articulo.img}" alt="Imagen de ${articulo.nombre}" style="max-width: 50px; max-height: 50px;"> - ${articulo.nombre} - $ ${articulo.precio} - <button class="btn btn-primary" onclick="restar(${index})">-</button> ${articulo.cantidad} <button class="btn btn-primary" onclick="sumar(${index})">+</button> - Subtotal $ ${(subtotalItem(articulo))}
                 </div>
                 <span class="fas fa-trash-alt" style="cursor: pointer;" onclick="eliminarDelCarrito(${index})"></span>
             </div>`;
@@ -75,6 +94,8 @@ function cerrarModal() {
 function eliminarDelCarrito(index) {
     carrito.splice(index, 1);
     actualizarListaCarrito();
+    mostrarCantItems();
+    sumarTotal();
     localStorage.setItem("carritoGuardado", JSON.stringify(carrito));
 }
 
@@ -96,16 +117,74 @@ function vaciarCarrito() {
         carrito.splice(0, carrito.length);
         localStorage.clear();
         actualizarListaCarrito();
+        mostrarCantItems();
+        sumarTotal();
 }
 
 function finalizarCompra() {
     let finCompra = document.getElementById('finalizar');
-    finCompra.innerHTML = `<p class= "blockquote">Será dirigido a nuestra página de pagos...</p>`
+    finCompra.innerHTML = `<p class= "blockquote" style="color: red; text-align: center;">Será dirigido a nuestra página de pagos...</p>`
     vaciarCarrito();
     setTimeout(() => {
         finCompra.innerHTML = '';
     }, 5000);
 }
+
+function sumar(index) {
+    carrito[index].cantidad++;
+    actualizarListaCarrito();
+    mostrarCantItems();
+    sumarTotal();
+}
+
+function restar(index) {
+    if (carrito[index].cantidad > 1) {
+        carrito[index].cantidad--;
+        actualizarListaCarrito();
+        mostrarCantItems();
+        sumarTotal();
+    }
+}
+
+function subtotalItem(articulo, index) {
+    let subtotal = articulo.precio * articulo.cantidad;
+    return subtotal
+}
+
+function sumarTotal() {
+    const subtotales = carrito.map((articulo, index) => subtotalItem(articulo, index));
+    const totalCompra = subtotales.reduce((acumulador, subtotal) => acumulador + subtotal, 0);
+    let mostrarTotal = document.getElementById('monto-total');
+    mostrarTotal.innerHTML = `<p class="blockquote" style="color: red; text-align: center;">Total de su compra: $ ${totalCompra}</p>`;
+}
+
+function mostrarIconoCarrito() {
+    const iconoCarrito = document.getElementById('lista-menu');
+    const contenedorCarrito = document.createElement('div');
+    contenedorCarrito.classList.add('carrito-container');
+    const i = document.createElement('i');
+    i.classList.add('bi', 'bi-cart', 'cart-icon');
+    i.onclick = mostrarModal;
+    const burbujaCantidad = document.createElement('span');
+    burbujaCantidad.classList.add('burbuja-cantidad');
+    burbujaCantidad.textContent = '0';
+    contenedorCarrito.appendChild(i);
+    contenedorCarrito.appendChild(burbujaCantidad);
+    iconoCarrito.appendChild(contenedorCarrito);
+}
+
+function mostrarCantItems() {
+    const burbujaCantidad = document.querySelector('.burbuja-cantidad');
+    const cantidadItems = carrito.reduce((acumulador, articulo) => acumulador + articulo.cantidad, 0);
+    if (cantidadItems > 0) {
+        burbujaCantidad.textContent = cantidadItems;
+        burbujaCantidad.style.display = 'block';
+    } else {
+        burbujaCantidad.style.display = 'none';
+    }
+}
+
+mostrarIconoCarrito();
 
 document.getElementById("buscadorArticulo").addEventListener("input", filtrarArticulos);
 
